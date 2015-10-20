9d6c50fd060f7baf308f34192ded0da003bedb48
Bruno Jouhier <bjouhier@gmail.com>
2015-10-20 23:40:04 +0200

    load from src if lib dir is missing

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 "use strict";
-module.exports = require('./lib/' + require('streamline-runtime').runtime);
+var fs = require('fs');
+var fsp = require('path');
+if (fs.existsSync(fsp.join(__dirname, 'lib'))) {
+	module.exports = require('./lib/' + require('streamline-runtime').runtime);
+} else {
+	module.exports = require('./src');	
+}
