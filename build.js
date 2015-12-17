85f7fd96b30db22e82356e8094af9fbfe28b36ef
Bruno Jouhier <bjouhier@gmail.com>
2015-12-17 10:12:57 +0100

    Sage/streamlinejs#312 - upgrade ez-streams to babel 6

--- build.js
+++ build.js
@@ -1,67 +1,19 @@
 "use strict";
 
 // This script rebuilds the lib/builtins-*.js files
-// It is run before publishing to NPM.
-// You must npm install babel and babel-plugin-streamline before running it.
-var babel = require('babel-core');
-require('babel-plugin-streamline');
-var fs = require('fs');
+"use strict";
 var fsp = require('path');
-
-var dirMode = parseInt('777', 8);
-
-function mkdirs(dir) {
-	if (fs.existsSync(dir)) return;
-	mkdirs(fsp.join(dir, '..'));
-	fs.mkdirSync(dir, dirMode);
-}
-
-function transform(src, dir, dst, map, runtime) {
-	var babelOptions = {
+var compile = require('streamline-helpers').compileSync;
+function options(runtime) {
+	return {
 		plugins: ['streamline'],
-		whitelist: [],
-		blacklist: [],
-		extra: {
-			streamline: {
-				runtime: runtime,
-				verbose: true,
-			},
-		},
+		runtime: runtime,
 	};
-	if (runtime === 'callbacks') babelOptions.whitelist.push('regenerator');
-	else babelOptions.blacklist.push('regenerator');
-	babelOptions.filename = src;
-	babelOptions.sourceFileName = src;
-	babelOptions.sourceMaps = true;
-	var source = fs.readFileSync(src, 'utf8');
-	var transformed =  babel.transform(source, babelOptions);
-	var code = transformed.code + '\n//# sourceMappingURL=' + map;
-	console.error("creating", fsp.join(dir, dst));
-	fs.writeFileSync(fsp.join(dir, dst), code, 'utf8');
-	fs.writeFileSync(fsp.join(dir, map), JSON.stringify(transformed.map, null, '\t'), 'utf8');
-}
-
-function build(src, dst, runtime) {
-	fs.readdirSync(src).forEach(function(name) {
-		var path = fsp.join(src, name);
-		var stat = fs.statSync(path);
-		mkdirs(dst);
-		if (stat.isDirectory()) {
-			var sub = fsp.join(dst, name);
-			build(path, sub, runtime);
-		} else if (/\._?js$/.test(name)) {
-			transform(path, 
-				dst,
-				name.replace('._js', '.js'), 
-				name.replace(/\._?js$/, '.map'),
-				runtime);			
-		}
-	});
 }
 
 ['callbacks', 'fibers', 'generators'].forEach(function(runtime) {
-	build(fsp.join(__dirname, 'src'), fsp.join(__dirname, 'lib', runtime), runtime);
+	compile(fsp.join(__dirname, 'src'), fsp.join(__dirname, 'lib', runtime), options(runtime));
 });
 ['callbacks', 'fibers'].forEach(function(runtime) {
-	build(fsp.join(__dirname, 'test'), fsp.join(__dirname, 'test-' + runtime), runtime);
+	compile(fsp.join(__dirname, 'test'), fsp.join(__dirname, 'test-' + runtime), options(runtime));
 });
--- test/index.js
+++ test/index.js
@@ -1,29 +1,5 @@
-"use strict";
-
-var fs = require('fs');
 var fsp = require('path');
-
-if (/[\\\/]test-fibers$/.test(__dirname) && /^4\./.test(process.versions.v8)) {
-	console.error("skip fibers test on node 4.x for now (fibers fails on travis CI)")
-	process.exit(0);
-}
-
-var tests = [];
-['common', 'server'].forEach(function(subdir) {
-	var root = fsp.join(__dirname, subdir);
-	fs.readdirSync(root).filter(function(file) {
-		return /\.js$/.test(file);
-	}).forEach(function(file) {
-		return tests.push(fsp.join(root, file));
-	});	
-});
-
-var testrunner = require("qunit");
-
-testrunner.run({
-	code: fsp.join(__dirname, 'loader.js'),
-    tests: tests,
-    maxBlockDuration: 10 * 1000,
-}, function(err) {
-	if (err) throw err;
+require('streamline-helpers').runTests({
+	root: __dirname,
+	subdirs: ['common', 'server'],
 });
